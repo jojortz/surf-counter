633ce3d9944737d1511c701895202dad8d9e73e5
jojortz <ortiz.jroman@gmail.com>
2025-03-19 11:21:35 -0700

    route title click to homepage

--- frontend/app/components/Navbar/Navbar.tsx
+++ frontend/app/components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import Container from "../Container";
 import NavbarMenu from "./NavbarMenu";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+    const router = useRouter();
     return (  
         <div
         className={`fixed w-full z-50 flex flex-col justify-center h-[60px] lg:h-[80px] py-4 bg-gradient-to-r from-purple-600 to-blue-500 shadow-md shadow-blue-500/50`}
@@ -12,7 +16,10 @@ const Navbar = () => {
             >
                 <Container>
                     <div className="flex items-center justify-between">
-                        <div className="text-2xl lg:text-4xl font-semibold">
+                        <div 
+                            className="text-2xl lg:text-4xl font-semibold cursor-pointer"
+                            onClick={() => router.push('/')}
+                            >
                             Surf Counter
                         </div>
                         <NavbarMenu/>
